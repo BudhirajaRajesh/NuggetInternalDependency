9a3c76532a5180744675fe511c0fac7bf0be4ebf
Rajesh Budhiraja <rajesh.budhiraja@zomato.com>
2025-04-22 15:26:00 +0530

    update dependencies

---
+++ Frameworks/NuggetFoundation.xcframework/ios-arm64/NuggetFoundation.framework/Headers/NuggetFoundation.h
@@ -0,0 +1,18 @@
+//
+//  NuggetFoundation.h
+//  NuggetFoundation
+//
+//  Created by Pranav Singh on 18/04/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NuggetFoundation.
+FOUNDATION_EXPORT double NuggetFoundationVersionNumber;
+
+//! Project version string for NuggetFoundation.
+FOUNDATION_EXPORT const unsigned char NuggetFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NuggetFoundation/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -18,18 +18,18 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "NuggetFoundation",
-            url: "https://github.com/BudhirajaRajesh/NuggetInternalDependency/releases/download/1.0.0-NuggetFoundation/NuggetFoundation.xcframework.zip",
-            checksum: "69ded32bf6c31cb0aed9b1b87186f8ebafb2f06ddf444f2efcee2557ae979a75"
+            url: "https://github.com/BudhirajaRajesh/NuggetInternalDependency/releases/download/1.0.1-NuggetFoundation/NuggetFoundation.xcframework.zip",
+            checksum: "8b8d4fa21157bee6f87d37da5103bee59636473eaf002e0b7bb93d0dd78611f0"
         ),
         .binaryTarget(
             name: "NuggetJumbo",
-            url: "https://github.com/BudhirajaRajesh/NuggetInternalDependency/releases/download/1.0.0-NuggetJumbo/NuggetJumbo.xcframework.zip",
-            checksum: "b6eba3873ea023c60c012f2c5db1f2bd072f949189be20c091da5720f80c8f98"
+            url: "https://github.com/BudhirajaRajesh/NuggetInternalDependency/releases/download/1.0.1-NuggetJumbo/NuggetJumbo.xcframework.zip",
+            checksum: "24d8f68d73dd195bf90f69b526237bf441dc47dcde83898af63f6f9fddd08d65"
         ),
         .binaryTarget(
             name: "NuggetApiManager",
-            url: "https://github.com/BudhirajaRajesh/NuggetInternalDependency/releases/download/1.0.0-NuggetAPIManager/ZApiManager.xcframework.zip",
-            checksum: "d9ae9426c22e234c729b3ba645d75a92c31c4bb25564856df1d3e3136ad92941"
+            url: "https://github.com/BudhirajaRajesh/NuggetInternalDependency/releases/download/1.0.1-NuggetAPIManager/ZApiManager.xcframework.zip",
+            checksum: "470b05483c2b497ef9df5e19a7561b4a921d634ecf981dfd24bffb020a37f219"
         ),
         .target(
             name: "NuggetInternalDependency",
